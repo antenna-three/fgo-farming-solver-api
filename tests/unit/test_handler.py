8809a491f330008ebf8a68945dfebe8da0b2ba19
antenna-three <tkduc@outlook.com>
2021-10-27 15:02:37 +0900

    Add CI workflow

--- tests/unit/test_handler.py
+++ tests/unit/test_handler.py
@@ -2,72 +2,53 @@ import json
 
 import pytest
 
-from hello_world import app
+from fgo_farming_solver import main
 
 
 @pytest.fixture()
 def apigw_event():
     """ Generates API GW Event"""
-
-    return {
-        "body": '{ "test": "body"}',
-        "resource": "/{proxy+}",
-        "requestContext": {
-            "resourceId": "123456",
-            "apiId": "1234567890",
-            "resourcePath": "/{proxy+}",
-            "httpMethod": "POST",
-            "requestId": "c6af9ac6-7b61-11e6-9a41-93e8deadbeef",
-            "accountId": "123456789012",
-            "identity": {
-                "apiKey": "",
-                "userArn": "",
-                "cognitoAuthenticationType": "",
-                "caller": "",
-                "userAgent": "Custom User Agent String",
-                "user": "",
-                "cognitoIdentityPoolId": "",
-                "cognitoIdentityId": "",
-                "cognitoAuthenticationProvider": "",
-                "sourceIp": "127.0.0.1",
-                "accountId": "",
-            },
-            "stage": "prod",
-        },
-        "queryStringParameters": {"foo": "bar"},
-        "headers": {
-            "Via": "1.1 08f323deadbeefa7af34d5feb414ce27.cloudfront.net (CloudFront)",
-            "Accept-Language": "en-US,en;q=0.8",
-            "CloudFront-Is-Desktop-Viewer": "true",
-            "CloudFront-Is-SmartTV-Viewer": "false",
-            "CloudFront-Is-Mobile-Viewer": "false",
-            "X-Forwarded-For": "127.0.0.1, 127.0.0.2",
-            "CloudFront-Viewer-Country": "US",
-            "Accept": "text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,*/*;q=0.8",
-            "Upgrade-Insecure-Requests": "1",
-            "X-Forwarded-Port": "443",
-            "Host": "1234567890.execute-api.us-east-1.amazonaws.com",
-            "X-Forwarded-Proto": "https",
-            "X-Amz-Cf-Id": "aaaaaaaaaae3VYQb9jd-nvCd-de396Uhbp027Y2JvkCPNLmGJHqlaA==",
-            "CloudFront-Is-Tablet-Viewer": "false",
-            "Cache-Control": "max-age=0",
-            "User-Agent": "Custom User Agent String",
-            "CloudFront-Forwarded-Proto": "https",
-            "Accept-Encoding": "gzip, deflate, sdch",
-        },
-        "pathParameters": {"proxy": "/examplepath"},
-        "httpMethod": "POST",
-        "stageVariables": {"baz": "qux"},
-        "path": "/examplepath",
-    }
+    with open('events/event.json', 'r', encoding='utf-8') as f:
+        return json.load(f)
 
 
 def test_lambda_handler(apigw_event, mocker):
-
-    ret = app.lambda_handler(apigw_event, "")
+    ret = main.handler(apigw_event, "")
     data = json.loads(ret["body"])
 
     assert ret["statusCode"] == 200
     assert "message" in ret["body"]
     assert data["message"] == "hello world"
     # assert "location" in data.dict_keys()
+
+
+@pytest.fixture()
+def query():
+    return {
+        "fields": "items,quests,id",
+        "objective": "ap",
+        "items": "00:100,01:100,02:100,03:100,08:100",
+        "ap_coefficients": "0:0.5",
+        "drop_merge_method": "add"
+    }
+
+def params():
+    return {
+        "fields": ["items", "quests", "id"],
+        "objective": "ap",
+        "items": {"00": "100", "01": "100", "02": "100", "03": "100", "08": "100"},
+        "ap_coefficients": {"0": "0.5"},
+        "drop_merge_method": "add"
+    }
+
+def test_decode_params(qurty, params):
+    decoded = main.decode_params(query)
+    assert decoded == params
+
+
+def test_validate_params(params):
+    main.validate_params(params, objective=('ap', 'lap'), drop_merge_method=('add', '1', '2'))
+    with pytest.raises(main.ParamError):
+        main.validate_params(params, objective=('ap', 'lap'), drop_merge_method=('1', '2'))
+
+
